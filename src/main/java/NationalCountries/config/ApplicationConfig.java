3f38744c92d83890ba52ad3861cd63d581e61e13
yasamingol <yasamingolzar.ce@gmail.com>
2024-07-07 04:19:40 -0700

    Remove user related files from national countries.

--- src/main/java/NationalCountries/config/ApplicationConfig.java
+++
@@ -1,9 +0,0 @@
-package NationalCountries.config;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableConfigurationProperties(AdminProperties.class)
-public class ApplicationConfig {
-}
--- src/main/java/NationalCountries/controllers/GlobalExceptionController.java
+++ src/main/java/NationalCountries/controllers/GlobalExceptionController.java
@@ -1,8 +1,6 @@
 package NationalCountries.controllers;
 
 import NationalCountries.exceptions.CountryNotFoundException;
-import NationalCountries.exceptions.JwtValidationException;
-import NationalCountries.exceptions.LoginException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,16 +12,5 @@ public class GlobalExceptionController {
     public ResponseEntity<String> handleCountryNotFoundException(CountryNotFoundException ex) {
         String errorMessage = "Error Code: " + ex.getStatusCode() + ", Message: " + ex.getMessage();
         return ResponseEntity.status(ex.getStatusCode()).body(errorMessage);
-    }
-
-    @ExceptionHandler(JwtValidationException.class)
-    public ResponseEntity<String> handleCountryNotFoundException(JwtValidationException ex) {
-        String errorMessage = "Error Code: " + ex.getStatusCode() + ", Message: " + ex.getMessage();
-        return ResponseEntity.status(ex.getStatusCode()).body(errorMessage);
-    }
-
-    @ExceptionHandler(LoginException.class)
-    public ResponseEntity<Object> handleLoginException(LoginException ex) {
-        return ex.getResponseEntity();
     }
 }
