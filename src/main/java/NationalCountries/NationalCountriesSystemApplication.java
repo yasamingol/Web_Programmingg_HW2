acf01fa4db8540f384fa89f084395f823038ef34
yasamingol <yasamingolzar.ce@gmail.com>
2024-07-07 05:05:29 -0700

    Remove user related files from national countries ane remove country related files from auth.

--- src/main/java/NationalCountries/NationalCountriesSystemApplication.java
+++ src/main/java/NationalCountries/NationalCountriesSystemApplication.java
@@ -3,11 +3,12 @@ package NationalCountries;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableCaching
 public class NationalCountriesSystemApplication {
 
